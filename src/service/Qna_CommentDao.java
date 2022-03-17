501e664ddd8dedcb1c8fe4b956768ca959664e2b
CJH <CJH@14.42.100.171>
2022-03-17 14:11:01 +0900

    22.03.17 // 게시글 답글 수 출력

--- src/service/Qna_CommentDao.java
+++ src/service/Qna_CommentDao.java
@@ -1,6 +1,8 @@
 package service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 
@@ -9,11 +11,27 @@ import util.MyBatisConnection;
 
 public class Qna_CommentDao {
 	private final static String ns = "qna_comment.";
+	private Map<String, Object> map = new HashMap<>();
 	
 	public List<Qna_Comment> selectComment(int refNum) {
 		SqlSession sqlSession = MyBatisConnection.getConnection();
 		try {
 			return sqlSession.selectList(ns + "selectComment",refNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			MyBatisConnection.close(sqlSession);
+		}
+		return null;
+	}
+	
+	public List countList(int pageInt, int limit) {
+		SqlSession sqlSession = MyBatisConnection.getConnection();
+		try {
+			map.clear();
+			map.put("start", (pageInt-1)*limit+1);
+			map.put("end", pageInt * limit);
+			return sqlSession.selectOne(ns + "count",map);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
